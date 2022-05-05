713f2c09571423170692f54e201945d34723236d
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2022-05-05 13:29:17 -0400

    updated misc scripts

--- Misc Tools/telomere_query.py
+++ Misc Tools/telomere_query.py
@@ -6,18 +6,35 @@ import argparse
 import os
 
 
-class Telo_fragment:
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input',  help="Path to genome file.fna", type=str)
+parser.add_argument('-b', '--begin', help="begining of range (0)",type=int)
+parser.add_argument('-e', '--end', help="end of slicing range (100000)",type=int)
+parser.add_argument('-s', '--step_size', help="interval of telomere slicing", type=int)
+parser.add_argument('-t', '--telo_seq', "telomere seuqence to be added", type=str)
+parser.add_argument('-o', '--output_path', help="Path for Output", type=str)
+
+args = parser.parse_args()
+
+
+
+
+class TeloFragment:
 
     def __init__(self, telo_sequence):
         self.telomere = telo_sequence
         self.telomere6 = telo_sequence*6
 
-    def genome2dict(self,input_path,filename,format):
+    def genome2dict(self, input_file_path):
+        # input_file_path: path of file
+
         lst=[]
         lst1=[]
-        if not input_path.endswith('/'):
-            input_path = input_path + '/'
-        for i in SeqIO.parse(f'{input_path}{filename}',format):
+        path = os.path.dirname(input_file_path)
+        self.genome_name = os.path.basename(input_file_path).replace('.fna','')
+        if not path.endswith('/'):
+            path = path + '/'
+        for i in SeqIO.parse(f'{input_file_path}', 'fasta'):
             chrom = i.id
             print(chrom)
             seq = str(i.seq)
@@ -25,28 +42,11 @@ class Telo_fragment:
             lst1.append(seq)
         self.mstr = dict((zip(lst,lst1)))
 
-    # def interval_gen(self, min, max, step_size):
-    #     self.interval = []
-    #     for i in range(min,max,step_size):
-    #         slice_set = [prior:i]
-    #         self.interval.append(i)
-        # for int in interval:
-        # list_ranges("N2",lst,range_lst,int,mstr,N2_min,N2_max)
-
-
     # slices chromosome by defined interval for chromosomal telomere fragemnts. Output is a dict
-    def telo_fragment_extract(self,Genome, arm, dictionary_of_seqs, start, end, step_size):
+    def telo_fragment_extract(self, Genome, arm, dictionary_of_seqs, start, end, step_size):
         dict = {}
-        # input vars :
-        # L_start = Left telomere start coordinate (e.g. 3000) (bp)
-        # R_end = right telomere end coordinate (e.g. 20924180) (bp)
-        # Chr_name = Chromosome Name (e.g. IL)
-        # for i, j, chr in zip(start, end, chr_name):
-        #     k = start[i]
-        #     l = end[j]
         prior = 0
         for j in dictionary_of_seqs:
-
             for i in range(start, end, step_size):
                 if arm == 'L':
                     L = dictionary_of_seqs[j][prior:i]
@@ -67,7 +67,8 @@ class Telo_fragment:
         dict["Telomere6"] = self.telomere6
         self.dict = dict
 
-    def file_write(self, dict, output_path):
+    @staticmethod
+    def file_write(dict, output_path):
         for i in (dict):
             filename = i
             if not output_path.endswith('/'):
@@ -77,32 +78,19 @@ class Telo_fragment:
             ofile.write(">" + i + "\n" + dict[i] + "\n")
             ofile.close()
 
-    def run(self,input_path,filename,format,Genome, arm, start, end, step_size, output_path):
-        self.genome2dict(input_path,filename,format)
-        self.telo_fragment_extract(Genome, arm, self.mstr, start, end, step_size)
+    def run(self, input_file_path, arm, start, end, step_size, output_path):
+        self.genome2dict(input_file_path)
+        self.telo_fragment_extract(self.genome_name, arm, self.mstr, start, end, step_size)
         self.file_write(self.dict, output_path)
 
-# C elegans
+
 def main():
-    for i in ('L','R'):
-        print(i)
-        telo = Telo_fragment('ttaggc')
-        telo.run("/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/"\
-            "genomes/Caenorhabditis Genomes/elegans/",
-                'CB4856.fna',
-                'fasta',
-                'CB4856',
-                 i,
-                 0,
-                 100000,
-                 5000,
-                 "/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/genomes/"\
-                 "Caenorhabditis Genomes/elegans/blastable index/")
+    for i in ('L', 'R'):
+        telo = Telo_fragment(args.telo_seq)
+        telo.run(args.input, args.begin, i, args.begin, args.end, args.step_size, ars.output_path)
 
-
-main()
-
-
+if __name__ == "__main__":
+    main()
 
 
 
