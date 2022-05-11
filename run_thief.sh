fb70d2930b61b2048132e0741a020fe1c7fe2118
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2022-05-11 11:17:33 -0400

    bug fix in main scripts

--- run_thief.sh
+++ run_thief.sh
@@ -11,14 +11,27 @@ done
 
 python __main__.py
 
+file_dir='Input_Files/Genomes/*'
 # loop through all files in dir
-for file in Input_files/Genomes/*; do
-  genome="$(basename "$file")" # basename with extension
-  genome_name="$(basename "$file" .fna)" # basename with no extension
-  pl="Input_Files/Fasta/L/${genome_name}/" # L dir
-  pr="Input_Files/Fasta/R/${genome_name}/" # R dir
+for file in $file_dir; do
+  echo $file
+  if [[ $file == *.fna ]]; then
+    genome="$(basename "$file")" # basename with extension
+    genome_name="$(basename "$file" .fna)" # basename with no extension
+    pl="Input_Files/Fasta/L/${genome_name}/" # L dir
+    pr="Input_Files/Fasta/R/${genome_name}/" # R dir
+    python funcs/main.py -pl ${pl} -pr ${pr} -t ${teloseq} -g ${genome}
+  elif [[ $file == *.fa ]]; then
+    genome="$(basename "$file")" # basename with extension
+    genome_name="$(basename "$file" .fa)" # basename with no extension
+    pl="Input_Files/Fasta/L/${genome_name}/" # L dir
+    pr="Input_Files/Fasta/R/${genome_name}/" # R dir
+    python funcs/main.py -pl ${pl} -pr ${pr} -t ${teloseq} -g ${genome}
+  else
+    echo 'Check File Extension'
+  fi
 done
 
 
-python funcs/main.py -pl ${pl} -pr ${pr} -t ${teloseq} -g ${genome}
+
 
