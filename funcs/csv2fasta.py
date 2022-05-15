c28331245ee42c6709629564556fd9fc6cea73a1
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2022-05-14 20:51:18 -0400

    found and fixed bug

--- funcs/csv2fasta.py
+++ funcs/csv2fasta.py
@@ -4,9 +4,13 @@ import pandas as pd
 # Csv from THIEF -> fasta
 def thief_csv2fasta(path_n_csv): #file and path
     # Extract filename
+    print(path_n_csv)
     filename = os.path.basename(path_n_csv)
+    print(filename)
     path = os.path.dirname(path_n_csv)
-    basename = filename.split('.')[0]
+    print(path)
+    basename = filename.replace('.csv','')
+    print(basename)
     df = pd.read_csv(path_n_csv)
     vals = [(w, x, y, z) for w, x, y, z in
             zip(df['value'], df['sandwhich_seq'], df['sandwhich_seq_L'], df['sandwhich_seq_R'])]
@@ -22,24 +26,4 @@ def thief_csv2fasta(path_n_csv): #file and path
         outfile.write(f'>right_sandwhich_{i[0]}\n')
         outfile.write(f'{vals[i[0]][3]}\n')
     outfile.close()
-
-
-# path = "/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/igv_sessions/" \
-#         "elegans igv/telo_fastas/THIEF_output/" \
-#        "2022_04_28__11_36_CB4856_Thief_C-elegans_output_table_for_blast.csv"
-#
-#
-# path = "/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/igv_sessions/" \
-#         "elegans igv/telo_fastas/THIEF_output/" \
-#        "2022_04_28__11_39_WB235_Thief_C-elegans_output_table_for_blast.csv"
-# thief_csv2fasta(path)
-#
-# path = "/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/igv_sessions/" \
-#         "elegans igv/telo_fastas/THIEF_output/" \
-#        "2022_04_28__11_39_WS274_Thief_C-elegans_output_table_for_blast.csv"
-# thief_csv2fasta(path)
-#
-# path = "/Users/mike/OneDrive - Maryville University/Ahmed Stuff/IGV_Telomeres/igv_sessions/Human/THIEF_output/" \
-#        "2022_04_28__11_43_Thief_Human_output_table_for_blast.csv"
-# thief_csv2fasta(path)
 
--- funcs/main.py
+++ funcs/main.py
@@ -7,29 +7,54 @@ from setup import set_up_dirs
 from thief import Thief
 from csv2fasta import thief_csv2fasta
 
+lst = ['Output_Files/',
+       'Output_Files/csv/',
+       'Output_Files/Blast_results/']
 
-GENOME_NAME = None
+for i in lst:
+    set_up_dirs(i)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-pl', '--Lpath',  help="Path containing L arm fastas", type=str)
 parser.add_argument('-pr', '--Rpath', help="Path containing R arm fastas",type=str)
-parser.add_argument('-t', '--telo_seq', help="telomere sequence e.g. 'ttaggg' ",type=str)
+parser.add_argument('-s', '--telo_seq', help="telomere sequence e.g. 'ttaggg' ",type=str)
 parser.add_argument('-g', '--genome', help="Genome File for BLAST analysis", type=str)
-# args = parser.parse_known_args()
-
-
-
-# parser.add_argument('-p',"--param1", help="First  Number for Operation",type=str)
-# parser.add_argument("param2", help="Second Number for Operation")
-# parser.add_argument("--power-to", help="Raise Addition of Parameters to the Power", type=int)
-
 args = parser.parse_args()
 
 
-LPATH = args.Lpath
-RPATH = args.Rpath
 
-TELO_SEQ = args.telo_seq
+
+
+
+LPATH = 'Input_Files/Fasta/L/c_elegans.PRJEB28388.WS274.genomic/'
+RPATH = 'Input_Files/Fasta/R/c_elegans.PRJEB28388.WS274.genomic/'
+TELO_SEQ = 'ttaggc'
+genome = 'c_elegans.PRJEB28388.WS274.genomic.fa'
+
+# LPATH = args.Lpath
+# RPATH = args.Rpath
+
+# if (args.genome).endswith('.fna'):
+#     GENOME_NAME = (args.genome).replace('.fna','')
+#     GENOME = args.genome
+# elif(args.genome).endswith('.fa'):
+#     GENOME_NAME = (args.genome).replace('.fa','')
+#     GENOME = args.genome
+# else:
+#     print('Error in main.py')
+
+
+if (genome).endswith('.fna'):
+    GENOME_NAME = (genome).replace('.fna','')
+    GENOME = genome
+elif(genome).endswith('.fa'):
+    GENOME_NAME = (genome).replace('.fa','')
+    GENOME = genome
+else:
+    print('Error in main.py')
+
+# TELO_SEQ = args.telo_seq
 if TELO_SEQ == 'ttaggc':
     ORGANISM = 'worms'
 elif TELO_SEQ == 'ttaggg':
@@ -37,14 +62,7 @@ elif TELO_SEQ == 'ttaggg':
 else:
     ORGANISM = 'unknown'
 
-if (args.genome).endswith('.fna'):
-    GENOME_NAME = (args.genome).replace('.fna','')
-    GENOME = args.genome
-elif(args.genome).endswith('.fa'):
-    GENOME_NAME = (args.genome).replace('.fa','')
-    GENOME = args.genome
-else:
-    print('check file extension')
+
 
 """
 Setup
@@ -56,21 +74,11 @@ blast scripts
 
 
 # THIEF structure
-if GENOME_NAME is None:
-    lst = ['Input_Files/',
-           'Input_Files/Genomes/',
-           'Input_Files/Fasta/',
-           'Input_Files/Fasta/L/',
-           'Input_Files/Fasta/R/',
-           'Output_Files/',
-           'Output_Files/csv/',
-           'Output_Files/Blast_results/']
-else:
-    lst = [f'Output_Files/Blast_results/{GENOME_NAME}',
-           f'Output_Files/Blast_results/{GENOME_NAME}/db_{GENOME_NAME}',
-           f'Input_Files/Fasta/L/{GENOME_NAME}',
-           f'Input_Files/Fasta/R/{GENOME_NAME}',
-           f'Output_Files/csv/{GENOME_NAME}']
+lst = [f'Output_Files/Blast_results/{GENOME_NAME}',
+       f'Output_Files/Blast_results/{GENOME_NAME}/db_{GENOME_NAME}',
+       f'Input_Files/Fasta/L/{GENOME_NAME}',
+       f'Input_Files/Fasta/R/{GENOME_NAME}',
+       f'Output_Files/csv/{GENOME_NAME}']
 for i in lst:
     set_up_dirs(i)
 
@@ -85,13 +93,15 @@ print(thief.out)
 sub_proc = subprocess.Popen([f'Rscript funcs/Filter.R -f {thief.out} -r {ORGANISM}'],shell=True, stdout=subprocess.PIPE)
 time.sleep(5)
 a = sub_proc.communicate()
-
+print(a)
 blast_file = f'{(thief.out).replace(".csv","")}_table_for_blast.csv'
+print(blast_file)
 
 thief_csv2fasta(blast_file)
-fasta4blast = blast_file.replace('.csv','.fasta')
 
-subprocess.run([f'bash funcs/blast_script.sh -g {GENOME_NAME} -f {fasta4blast}'], shell=True)
+fasta4blast = blast_file.replace('.csv','.fasta')
+print(fasta4blast)
+subprocess.run([f'bash funcs/blast_script.sh -g {GENOME} -f {fasta4blast}'], shell=True)
 blast_output = f'Output_Files/Blast_results/{GENOME_NAME}/{GENOME_NAME}_blastn.txt'
 
 subprocess.call(f'Rscript funcs/blast_column_names.R -f {blast_output}', shell=True)
